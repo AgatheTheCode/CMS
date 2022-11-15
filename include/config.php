b87a5393a870edc936e5fd6cc43a9b83a74250b3
Agathe Dufour <agathe.dufour.marin@gmail.com>
2022-11-15 09:56:41 +0100

    modified:   include/config.php

--- include/config.php
+++ include/config.php
@@ -3,9 +3,17 @@
 function connexion()
 {
   //connection à la base de données en pdo
+  //condition de vérificaiton de l'environnement de développement / production
   //syntaxe $pdo = new PDO('mysql:host=serveur;dbname=nom de la base de données', 'nom d'utilisateur', 'mot de passe');
-  $pdo = new PDO('mysql:host=localhost;dbname=CMS_test;charset=utf8', 'root', '');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+  if (str_contains($_SERVER['SERVER_NAME'], 'NOM_DU_SERVEUR')) {
+    $pdo = new PDO('mysql:host=ADRESSE_DU_SERVEUR;dbname=nom_de_la_base;charset=utf8', 'nom_utilisateur', 'mot_de_passe'); //à modifier
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} else {
+    //local
+    $pdo = new PDO('mysql:host=localhost;dbname=biblio;charset=utf8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
 
   if ($pdo) {
     return $pdo;
